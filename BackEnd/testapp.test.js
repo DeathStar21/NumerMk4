91fb420971f2701b105b017b5a452914f6a793a3
Jatunatthaphong <qlergaming@gmail.com>
2023-04-11 17:26:21 +0700

    commit

--- BackEnd/testapp.test.js
+++ BackEnd/testapp.test.js
@@ -11,6 +11,7 @@ test('GET / should return "Hello Qler"', async () => {
 });
 
 const secretKeys = '12345';
+
 describe('GET /createToken/:admin', () => {
   test('should return a JWT token', async () => {
     const res = await request(app).get('/createToken/Qler').expect(200);
@@ -21,3 +22,9 @@ describe('GET /createToken/:admin', () => {
   });
 });
 
+test('Querry', async()=>{
+  const response = await request(app).get('/bisection');
+  expect(response.statusCode).toBe(200);
+  expect(response.text).toBe('[{\"idBisection\":1,\"Equation\":\"x-13^(1/4)\",\"XL\":1.5,\"XR\":2},{\"idBisection\":2,\"Equation\":\"x^3-x-2\",\"XL\":1,\"XR\":2},{\"idBisection\":3,\"Equation\":\"x^4-x-10\",\"XL\":2,\"XR\":5},{\"idBisection\":4,\"Equation\":\"y^4+1+2\",\"XL\":4,\"XR\":5}]');
+
+})
